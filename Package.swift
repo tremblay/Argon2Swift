9544cb434676e959bdb0887d2bff5498ed036d1e
Colin Tremblay <colin@withuno.com>
2021-02-23 15:29:57 -0700

    working locally without the dependency on argon2 changes

--- Package.swift
+++ Package.swift
@@ -16,18 +16,45 @@ let package = Package(
             targets: ["Argon2Swift"]
         ),
     ],
-    dependencies: [
-        .package(
-            name: "argon2", 
-            url: "https://github.com/tremblay/phc-winner-argon2.git", .branch("swift-package-manager")
-            //path: "../phc-winner-argon2"
-        )
-    ],
     targets: [
-/*        .systemLibrary(
+        .target(
             name: "argon2",
-            path: "Sources/Argon2/src"
-        ),*/
+            path: "Sources/Argon2",
+            exclude: [
+                "kats",
+                "vs2015",
+                "latex",
+                "libargon2.pc.in",
+                "export.sh",
+                "appveyor.yml",
+                "Argon2.sln",
+                "argon2-specs.pdf",
+                "CHANGELOG.md",
+                "LICENSE",
+                "Makefile",
+                "man",
+                "README.md",
+                "src/bench.c",
+                "src/genkat.c",
+                "src/opt.c",
+                "src/run.c",
+                "src/test.c",
+            ],
+            sources: [
+                "src/blake2/blake2b.c",
+                "src/blake2/blake2.h",
+                "src/blake2/blake2-impl.h",
+                "src/blake2/blamka-round-ref.h",
+                "src/argon2.c",
+                "src/core.c",
+                "src/core.h",
+                "src/encoding.c",
+                "src/encoding.h",
+                "src/ref.c",
+                "src/thread.c",
+                "src/thread.h",
+            ]
+        ),
         .target(
             name: "Argon2Swift",
             dependencies: ["argon2"],
